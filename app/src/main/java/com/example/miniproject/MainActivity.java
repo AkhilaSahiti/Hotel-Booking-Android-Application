5e0754f0a89c6ca2a8140e7b580188901d232b45
AkhilaSahiti <racherlasahiti@gmail.com>
2020-01-06 00:51:51 +0530

    Main Screen

---
+++ app/src/main/java/com/example/miniproject/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.miniproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button room = findViewById(R.id.rooms);
+        room.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RatesOfRooms.class);
+                startActivity(intent);
+            }
+        });
+        Button facilities= findViewById(R.id.facilites);
+        facilities.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Facilities.class);
+                startActivity(intent);
+            }
+        });
+        Button booking= findViewById(R.id.booking);
+        booking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Booking.class);
+                startActivity(intent);
+            }
+        });
+        Button rates= findViewById(R.id.rates);
+        rates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,RoomAvailability.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
